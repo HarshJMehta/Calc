79a2a2ae6c887b85f4e800e06bf200b34555a1bd
Harsh Mehta <harshmehta@Harshs-MacBook-Pro.local>
2017-09-26 10:15:52 -0400

    With comments

--- Calc/ViewController.swift
+++ Calc/ViewController.swift
@@ -1,7 +1,10 @@
 //
 //  ViewController.swift
 //  Calc
-//
+//Name:Harsh Mehta
+//ID:300951815
+//Last Modified: 26 Sept 17
+//Program Description:It is a basic calculat app which performs basic arithmetic functions.
 //  Created by Student on 2017-09-20.
 //  Copyright © 2017 CENTE. All rights reserved.
 //
@@ -12,12 +15,12 @@ class ViewController: UIViewController {
     
     var numberTobedisplayed:Double = 0;
      var storenumber:Double = 0;
-    var operations = false;
+    var operations = false;/* Variable to be perform all the basic arithmetic opeerations*/
     var operationsToBePerformed = 0;
 
     @IBOutlet weak var label: UILabel!
     
-    @IBAction func Button(_ sender: UIButton)
+    @IBAction func Button(_ sender: UIButton) /*Function that does the processing of all the numbers*/
     {
         if operations == true
         {
@@ -34,24 +37,24 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func Function(_ sender: UIButton)
+    @IBAction func Function(_ sender: UIButton) /* Function for basic operations*/
     {
         if label.text != "" && sender.tag != 11 && sender.tag != 16
         {
           storenumber = Double(label.text!)!
-                if sender.tag == 12
+                if sender.tag == 12 /*For divide */
                  {
                     label.text = "/"
                  }
-                 else if sender.tag == 13
+                 else if sender.tag == 13 /*For multiplication */
                  {
                 label.text = "X"
                  }
-                else if sender.tag == 14
+                else if sender.tag == 14 /*For subtraction */
                  {
                 label.text = "-"
                  }
-                else if sender.tag == 15
+                else if sender.tag == 15/*For addition */
                 {
                     label.text = "+"
                 }
@@ -76,7 +79,7 @@ class ViewController: UIViewController {
                 {
                      label.text = String(storenumber + numberTobedisplayed)
                 }
-                else if sender.tag == 11
+                else if sender.tag == 11 /*For Clear*/
                 {
                     label.text = ""
                     storenumber = 0;
